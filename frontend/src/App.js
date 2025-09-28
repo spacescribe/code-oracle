dfbafcf21fa06678a12d4759b5c7815f272a9a3d
spacescribe <nandini2417@gmail.com>
2025-09-28 19:39:05 +0530

    Added index repo to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Search from './components/Search';
+import IndexRepo from "./components/IndexRepo";
 import './App.css';
 
 function App() {
   return (
     <div className='app-container'>
       <h2>Code-Oracle 🔮</h2>
+
+      <IndexRepo />
       <Search />
     </div>
   );
---
+++ frontend/src/components/IndexRepo.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function IndexRepo() {
+    const [repoUrl, setRepoUrl] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [msg, setMsg] = useState("")
+
+    const handleIndex = async () => {
+        if(!repoUrl.trim()) return;
+
+        setLoading(true)
+        setMsg("")
+
+        try{
+            const res = await axios.post("http://127.0.0.1:8000/index-repo", {
+                repo_url: repoUrl,
+            })
+
+            setMsg(`Repo: "${res.data.repo}" indexed. ${res.data.files} files.`)
+            setRepoUrl("");
+        } catch (err){
+            console.error(err)
+            setMsg(`Failed to index repo "${repoUrl}"`)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return(
+        <div className='index-repo-container'>
+            <input type="text" value={repoUrl} onChange={(e) => setRepoUrl(e.target.value)} placeholder='Enter git repo url..' />
+            <button onClick={handleIndex} disabled={loading}> {loading ? "Indexing...": "Index repo"} </button>
+            {msg && <p className='message'>{msg}</p>}
+        </div>
+    )
+}
+
+export default IndexRepo;
